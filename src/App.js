20f48096f53f30615fd9e097083c33a493cb2caa
Bethsyf <bethsyfalcon@gmail.com>
2022-04-18 14:15:29 -0500

    carpetas para proyecto treact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import List from "../components/List";
+import styled from "styled-components"
+
+const StyledContainer = styled.div`
+  background-color: #121227;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledContainer>
+      <List />
+    </StyledContainer>
   );
 }
 
